9a2c740e8b78970fd19346838b72e97f80e76be8
Aderamas <nabil.boubekri24@gmail.com>
2024-10-25 15:29:55 +0200

    Test attente : Raté

--- Test_Unitaire_1_TestClasse/UnitTest1.cs
+++ Test_Unitaire_1_TestClasse/UnitTest1.cs
@@ -99,7 +99,7 @@ namespace Test_Unitaire_1_TestClasse
         {
             var mock = new Mock<User>() { CallBase = true };
             mock.Setup(x => x.save()).Returns(true);
-            mock.Object.email = "User@gmail.com";
+            mock.Object.email = "User@gmailcom";
             mock.Object.prenom = "UserName";
             mock.Object.nom = "UserLastName";
             mock.Object.password = "aLABALALA123";
